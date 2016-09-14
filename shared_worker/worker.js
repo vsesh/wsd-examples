5c0e5b6f7932f7829d0850b39a10cb44282f8e39
vsesh <v.shmyroff@ya.ru>
2016-09-14 12:23:21 +0300

    Shared Worker example

---
+++ shared_worker/worker.js
@@ -0,0 +1,24 @@
+
+var ports = [];
+self.addEventListener('connect', function(e) {
+    console.log(
+        'new connection!'
+    );
+    setupInterval();
+    ports.push(e.ports[0]);
+});
+
+var intervalID;
+function setupInterval () {
+    if (!intervalID) {
+        setInterval(function () {
+            sendToAll('>> ' + Math.random());
+        }, 1000);
+    }
+}
+
+function sendToAll(message) {
+    ports.forEach(function (port) {
+        port.postMessage(message);
+    });
+}
